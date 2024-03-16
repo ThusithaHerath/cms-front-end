d897ce89fe4c21ec4219bdbcf24e3a56dcd04ba2
Thusitha Herath <thusithalakshanherath@gmail.com>
2024-03-16 15:31:27 +0530

    initial commit

--- src/Views/index.jsx
+++ src/Views/index.jsx
@@ -6,7 +6,7 @@ export default function UnknownView() {
       <div className="flex flex-col gap-4 text-center">
         <p className="text-[70px] font-bold text-gray-500">Oops!</p>
         <p className="text-lg font-normal uppercase">
-          This page could not be found
+          This page could not be found !
         </p>
         <a href="/" className="bg-blue-500 p-2 rounded-lg text-white mt-4">
           Go To Homepage
